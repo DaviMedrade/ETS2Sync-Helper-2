6dc3e4391e3765f1668e0c64b753d265fef03b61
David Santos <david@dsantosdev.com>
2016-06-01 09:31:09 -0300

    Build script: require lang parameter; place binary in build dir

--- build_exe.bat
+++ build_exe.bat
@@ -1,1 +1,11 @@
-ocra --gem-minimal --icon src/res/app.ico --no-lzma --windows --chdir-first src/ets2sync_helper.rb src/res/*
+@ECHO OFF
+IF [%1] == [] GOTO no_lang
+
+ECHO ON
+ocra --gem-minimal --icon src/res/app.ico --no-lzma --windows --output build/ets2sync_helper_%1.exe --chdir-first src/ets2sync_helper.rb src/res/* -- %1
+@ECHO OFF
+GOTO :eof
+
+:no_lang
+ECHO No language specified. Please pass the language as a command-line argument. E.g:
+ECHO     build_exe pt-BR
