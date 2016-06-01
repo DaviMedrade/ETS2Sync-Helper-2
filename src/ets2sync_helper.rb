d0c93b18313d72a99b733faa6fe0bb164f7f8c89
David Santos <david@dsantosdev.com>
2016-06-01 07:57:41 -0300

    Remove commented-out debug code

--- src/ets2sync_helper.rb
+++ src/ets2sync_helper.rb
@@ -1,14 +1,8 @@
 begin
-	#puts "PATH: #{ENV['PATH'].inspect}\n\n"
-	#puts "PATH encoding: #{ENV['PATH'].encoding.inspect}\n\n"
-	#puts "dirname: #{File.dirname(__FILE__).inspect}\n\n"
-	#puts "dirname encoding: #{File.dirname(__FILE__).encoding.inspect}\n\n"
-	#puts "Loading..."
 	APP_NAME = "ETS2Sync Helper"
 	Dir.chdir(__dir__)
 	require_relative "lib/ets2sync_helper"
 
-	#puts "Starting app..."
 	app = Qt::Application.new(ARGV)
 	ETS2SyncHelper.new
 	app.exec
