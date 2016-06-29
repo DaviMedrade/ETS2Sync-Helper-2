ff5d01061006e45ef035e23811328f1d096ef057
David Santos <david@dsantosdev.com>
2016-06-29 19:04:06 -0300

    Scripts don't need language as a paramenter anymore

--- build_exe.bat
+++ build_exe.bat
@@ -1,11 +1,1 @@
-@ECHO OFF
-IF [%1] == [] GOTO no_lang
-
-ECHO ON
-ocra --gem-minimal --icon src/res/app.ico --no-lzma --windows --output build/ets2sync_helper_%1.exe --chdir-first src/ets2sync_helper.rb src/res/* -- %1
-@ECHO OFF
-GOTO :eof
-
-:no_lang
-ECHO No language specified. Please pass the language as a command-line argument. E.g:
-ECHO     build_exe pt-BR
+ocra --gem-minimal --icon src/res/app.ico --no-lzma --windows --output build/ets2sync_helper.exe --no-autoload --chdir-first src/ets2sync_helper.rb src/res/*
