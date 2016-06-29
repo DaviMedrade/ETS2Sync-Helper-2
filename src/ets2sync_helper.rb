1071fd0a8a0d5f65943a0db91e879b85f60f4d83
David Santos <david@dsantosdev.com>
2016-06-29 19:01:18 -0300

    Add language helper methods and ETS2SyncHelper::restart!, which restarts the program

--- src/ets2sync_helper.rb
+++ src/ets2sync_helper.rb
@@ -26,6 +26,34 @@ begin
 
 		def self.language ; @settings[:language] ; end
 
+		def self.available_languages
+			langs = {}
+			MSGS.each do |lang, msgs|
+				langs[lang] = msgs[:this_language]
+			end
+			langs
+		end
+
+		def self.effective_language_for(lang)
+			return lang if MSGS.has_key?(lang)
+			lang_s = lang.to_s
+			if lang_s.include?("-")
+				lang = lang_s.partition("-").first.to_sym
+				return lang if MSGS.has_key?(lang)
+			end
+			:en
+		end
+
+		def self.restart!
+			sleep(0.5) # let the process finish what it must
+			if ENV["OCRA_EXECUTABLE"]
+				spawn(ENV["OCRA_EXECUTABLE"])
+			else
+				spawn(RbConfig.ruby, Pathname($0).basename.to_s)
+			end
+			exit(0)
+		end
+
 		MSGS = {}
 		MSG = {}
 		Dir.glob("lang/*rb") do |f|
@@ -33,13 +61,7 @@ begin
 		end
 
 		MSG.default_proc = proc do |h, k|
-			lang = self.language
-			unless MSGS.has_key?(lang)
-				lang_s = lang.to_s
-				if lang_s.include?("-")
-					lang = lang_s.partition("-").first.to_sym
-				end
-			end
+			lang = effective_language_for(self.language)
 			if MSGS.has_key?(lang) && MSGS[lang].has_key?(k)
 				MSGS[lang][k]
 			else
