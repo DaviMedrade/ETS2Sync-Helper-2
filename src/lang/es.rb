6af0eabfc8ddee17fdb70604f864de34241ae0e9
Jhonathan Andrade Ramos <profissional.ramos@gmail.com>
2016-06-30 13:24:01 -0300

    Adding new string "about_icons"
    * adding new string "about_icons" to Deutch and Spanish language files;

--- src/lang/es.rb
+++ src/lang/es.rb
@@ -1,12 +1,13 @@
-ETS2SyncHelper::MSGS[:es] = {
+MSGS[:es] = {
 	# in 'about_translator', put your own name and a URL of your choosing (though I'll have to approve it)
 	about_translator: "Traducción al español por <a href='https://www.facebook.com/AnjodaNoiteAna'>Ana Pereira</a>.",
 	about_button: "Sobre…",
 	about_title: "Sobre el %s", # %s is the application name
-	about_author: "Criado por <a href='https://github.com/davidsantos-br'>David Santos</a>.",
+	about_author: "Criado por <a href='https://www.facebook.com/AnjodaNoiteAna'>Ana Pereira</a>.",
 	about_api: "Lista de trabajos proporcionada por <a href='http://www.ets2sync.com/'>ETS2 Job Sync</a>.",
 	about_ruby: "<a href='http://ruby-lang.org/'>Ruby #{RUBY_VERSION}</a> — proporcionada por <a href='http://rubyinstaller.org/'>RubyInstaller</a>.",
 	about_qt: "<a href='https://www.qt.io/'>Qt %s</a> — proporcionada por <a href='https://rubygems.org/gems/qtbindings'>qtbindings</a>.", # %s is the Qt version
+	about_icons: "Los iconos de banderas de los países proporcionados por <a href='http://www.famfamfam.com/'>Mark James</a>.",
 	about_website: "Sitio web: %s",
 	check_again: "Revisar otra vez",
 	checking: "En busca de nuevas versiones…",
